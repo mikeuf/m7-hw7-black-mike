2d7400981f90c96362fa687ea5a9c98c25d46c60
Mike <mblack101@ufl.edu>
2019-10-09 00:20:46 -0400

    Add DOM updating code to javascript

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,16 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
 
+      var welcomeHeader = document.getElementById('welcome-header');
+      welcomeHeader.insertAdjacentText('beforeend', apiResult.name);
+
+      var weatherMessage = document.getElementById('weather-message');
+      var weatherDescription = document.createTextNode(apiResult.weather[0].description);
+      weatherMessage.appendChild(weatherDescription);
     }
 };
+
+
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=32713,us&appid=6efff70fe1477748e31c17d1c504635f', true);
 xmlhttp.send();
 
